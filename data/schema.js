2f204b5582c6e1986bb8e768fd165345610097bc
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2018-10-12 16:44:30 -0400

    Schema done

--- data/schema.js
+++ data/schema.js
@@ -1,9 +1,77 @@
+import {MongoClient, ObjectId} from 'mongodb';
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 import mocks from './mocks';
 
+const MONGO_URL = 'mongodb://localhost:27017/quip'
+
 const typeDefs = `
+type Coordinate {
+  xyarr: [Float]
+}
+
+type Coordinates {
+  coordinateArr: [Coordinate]
+}
+
+type Geometry {
+  type: String
+  coordinates: [Coordinates]
+}
+
+type NV {
+  name: String
+  value: Float
+}
+
+type Scalar_Features {
+  ns: String
+  nv: [NV]
+}
+
+type Properties {
+  scalar_features: [Scalar_Features]
+}
+
+type Analysis {
+  execution_id: String
+  study_id: String
+  source: String
+  computation: String
+}
+
+type Image {
+  case_id: String
+  subject_id: String
+}
+
+type Provenance {
+  image: Image
+  analysis: Analysis
+  data_loader: String
+  batch_id: String
+  tag_id: String
+}
+
+type Object {
+  _id: ID
+  type: String
+  parent_id: String
+  randval: Float
+  creation_date: String
+  object_type: String
+  x: Float
+  y: Float
+  normalized: Boolean
+  bbox: [Float]
+  geometry: Geometry
+  footprint: Int
+  properties: Properties
+  provenance: Provenance
+  submit_date: String
+}
+
 type Query {
-  testString: String
+  
 }
 `;
 
--- server.js
+++ server.js
@@ -2,11 +2,12 @@ import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
+import cors from 'cors';
 
 const GRAPHQL_PORT = 3000;
 
 const graphQLServer = express();
-
+graphQLServer.use(cors());
 graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
